aefedceb7491d6d5ed744421edd26713e2f0039d
jeonjaemin <smily094@stninfotech.com>
2026-05-13 16:03:30 +0900

    chore: remove obsolete debug, test, and utility scripts

--- src/utils/test_comparison.cjs
+++
@@ -1,9 +0,0 @@
-const { layoutsEqual } = require('./test_comp_helper.cjs');
-
-const a = [{ rackId: '1', position: [0, 0] }];
-const b = [{ rackId: '1', position: [0, 1] }];
-const c = [{ rackId: '1', position: [0, 0] }, { rackId: '2', position: [1, 1] }];
-
-console.log("a vs b:", layoutsEqual(a, b)); // expected false
-console.log("a vs c:", layoutsEqual(a, c)); // expected false
-console.log("a vs a:", layoutsEqual(a, JSON.parse(JSON.stringify(a)))); // expected true
